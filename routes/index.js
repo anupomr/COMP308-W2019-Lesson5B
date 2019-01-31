299fa139dd5bd23c9e6c363c03a4e4f379a131bb
Anupom Roy <anupomr@yahoo.com>
2019-01-31 13:54:30 -0500

    Modified UI

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let router = express.Router();
-
+const session = require('express-session');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/logout', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 router.get('/feedback', function(req, res, next) {
-  res.render('feedback', { title: 'FeedBack' });
+  res.render('index', { title: 'FeedBack', body:'please Login' });
 });
 router.post('/', function(req, res, next) {
   res.render('feedback', { title: 'FeedBack' });
