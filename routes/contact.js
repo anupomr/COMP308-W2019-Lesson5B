c178f8b450a51ed264ca8693aa500e11fcaaa947
Anupom Roy <anupomr@yahoo.com>
2019-02-10 00:55:16 -0500

    Add crearte functionality in MongoDB

--- routes/contact.js
+++ routes/contact.js
@@ -1,23 +1,48 @@
 let express = require('express');
 let router = express.Router();
 
-let mongoose=require('mongoose');
+let mongoose = require('mongoose');
 //Create a reference to the db schema
-let contact=require('../models/contact');
+let contactModel = require('../models/contact');
 
 //Read
-router.get('/', (req,res,next)=>{
-    contact.find((err, contactList)=>{
-       if(err){
-           return console.error(err);
-       } 
-       else{
-           //console.log(contactList);
-           res.render('contacts/index',{
-               title: 'Contact List',
-               contactList: contactList
-           });
-       }
+router.get('/', (req, res, next) => {
+    contactModel.find((err, contactList) => {
+        if (err) {
+            return console.error(err);
+        }
+        else {
+            //console.log(contactList);
+            res.render('contacts/index', {
+                title: 'Contact List',
+                contactList: contactList
+            });
+        }
+    });
+});
+
+/** Get router for add page */
+router.get('/add', (req, res, next) => {
+    res.render('contacts/add', {
+        title: 'Add new Contact'
+    });
+});
+/* POST route for processing add page */
+router.post('/add', (req, res, next) => {
+    // console.log(req.body);
+    let newContact = contactModel({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+    contactModel.create(newContact, (err, contactModel) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/contact-list')
+        }
     });
 });
 
