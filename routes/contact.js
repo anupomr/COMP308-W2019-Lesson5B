ef825ff14ebeadda1148e50f1117d4adcee0a931
Anupom Roy <anupomr@yahoo.com>
2019-02-11 13:34:23 -0500

    Edit functionality added

--- routes/contact.js
+++ routes/contact.js
@@ -43,6 +43,41 @@ router.post('/add', (req, res, next) => {
         else{
             res.redirect('/contact-list')
         }
+    });   
+});
+
+router.get('/edit/:id',(req,res,next)=>{
+    let id=req.params.id;
+    contactModel.findById(id,(err,contactObject)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.render('contacts/edit',{
+                title:'Edit Contact',
+                contact: contactObject
+            });
+        }
+    });
+});
+router.post('/edit/:id', (req,res,next)=>{
+    let id=req.params.id;
+    let updatedContact=contactModel({
+        "_id":id,
+        "firstName":req.body.firstName,
+        "lastName":req.body.lastName,
+        "age":req.body.age
+    });
+    contactModel.update({_id:id},updatedContact,(err)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //refresh the Contact
+            res.redirect('/contact-list');
+        }
     });
 });
 
